53af608b5f1dfb2f42e76fb822002e2d7e1319b7
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-02-20 11:20:30 +0530

    Add Main class with pizza ordering functionality

--- source/Main.java
+++ source/Main.java
@@ -1,1 +1,22 @@
 
+public class main {
+
+	public static void Main(String[] args) {
+		boolean veg;
+		pizza basepizza=new pizza(true);
+		deluxepizza dp=new deluxepizza(true);
+		
+		
+		basepizza.addextracheese();
+		basepizza.addextratoppings();
+		basepizza.takeaway();
+		basepizza.getbill();
+		
+		System.out.println("-----------------");
+		dp.getbill();
+		
+
+	}
+
+}
+
