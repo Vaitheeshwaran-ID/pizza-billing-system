1ab5a4c8783f32362075474239c2f13e6758dc1c
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-02-20 11:22:37 +0530

    Implement Deluxepizza class with extra features

--- source/Deluxepizza.java
+++ source/Deluxepizza.java
@@ -1,1 +1,18 @@
+public class Deluxepizza extends pizza{
+
+	public deluxepizza(Boolean veg) {
+		super(veg);
+		super.addextracheese();
+		super.addextratoppings();
+	}
+
+	@Override
+	public void addextracheese() {
+		
+	}
+	@Override
+	public void addextratoppings() {
+		
+	}
+}
 
