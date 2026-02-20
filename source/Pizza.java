30f2f214bd98f7863be320b0216c45b1eb3ec7c7
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-02-20 11:43:37 +0530

    Refactor Pizza class for base price handling
    Refactored Pizza class to separate base price from total price and updated bill calculation.

--- source/Pizza.java
+++ source/Pizza.java
@@ -1,5 +1,6 @@
 
 public class Pizza {
+private int baseprice;
 private int price;
 private Boolean veg;
 
@@ -7,69 +8,51 @@ private int backpack=20;
 private int extracheeseprice=100;
 private int extratoppingprice=150;
 
-
 private boolean isextracheeseadded=false;
 private boolean isextratoppingsadded=false;
 private boolean isoptedfortakeaway=false;
-
-
-
 
 public pizza(Boolean veg) {
 	this.veg=veg;
 	
 if(this.veg) {
-	this.price=300;
+	this.baseprice=300;
 }
 else {
-	this.price=400;
+	this.baseprice=400;
 }
+price=baseprice;
 
 }
-
 
 public void addextracheese() {
 	isextracheeseadded=true;
 	this.price+=extracheeseprice;
 }
-
 public void addextratoppings() {
 	isextratoppingsadded=true;
 	this.price+=extratoppingprice;
 }
-
 public void takeaway() {
 	isoptedfortakeaway=true;
 this.price+=backpack;
 }
-
-
-public void getbill() {
+public void getbill() {	
+	System.out.println("Base Price:"+baseprice);
 	
-	System.out.println("Base Price:"+price);
-
 	if(isextracheeseadded) {
 		System.out.println("Extra cheese added:"+ extracheeseprice);
-
 	}
 	
 	if(isextratoppingsadded) {
 				System.out.println("Extra toppings added:"+ extratoppingprice);
-
 	}
 	
 	if(isoptedfortakeaway) {
 		System.out.println("backpack price:"+ backpack);
-
 	}
 	
-	System.out.println(price);
+	System.out.println("Total Bill:"+price);
 }
-
-
-
-
-
 }
-
 
